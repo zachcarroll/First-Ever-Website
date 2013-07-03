7ffd732fedfb4be88dfa89ca7414f967d55a38d2
zachcarroll <carroll.zach@gmail.com>
2013-07-03 13:00:18 -0400

    revert personal page

--- personal page/README.md
+++
@@ -1,2 +0,0 @@
-zachcarroll.github.io
-=====================
--- personal page/script.js
+++
@@ -1,4 +0,0 @@
-$('a.thailand').on('click',function(event){
-	event.preventDefault();
-	$('div.lightboxContainer').css('display','block');
-})
