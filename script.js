76ab55fc8e19bf6f6fbc1677654789ce94c761b7
zachcarroll <carroll.zach@gmail.com>
2013-07-03 15:49:40 -0400

    updated navbar for all pages, getting closer with the from scratch lightbox

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-$('a.thailand').on('click',function(event){
-	event.preventDefault();
-	$('div.lightboxContainer').css('display','block');
-})
+$(function() {
+	$('a.thailand').on('click',function(event){
+		event.preventDefault();
+		var lightbox = $('div.lightbox');
+		var lightboxContainerAndBg = $('div.lightboxContainer,div.lightboxContainerBg');
+		lightboxContainerAndBg.css('display','block');
+		lightbox.html('<img src="' + $(this).attr('href') + '" />');
+		
+		lightbox.on('click',function() {
+			$(this).css('display','none');
+			lightboxContainerAndBg.css('display','none');
+		});
+	});
+});
