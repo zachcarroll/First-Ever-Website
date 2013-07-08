e8fd6126c40e90f108b2abcc40e633e78155c4ac
zachcarroll <carroll.zach@gmail.com>
2013-07-08 13:07:13 -0400

    dropdown menu work

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ $(function() {
 	$('a.thailand').on('click',function(event){
 		event.preventDefault();
 		var lightbox = $('div.lightbox');
-		var lightboxContainerAndBg = $('div.lightboxContainer,div.lightboxContainerBg');
-		lightboxContainerAndBg.css('display','block');
+		var lightboxContainer = $('div.lightboxContainer');
+		lightboxContainer.css('display','block');
 		lightbox.html('<img src="' + $(this).attr('href') + '" />');
 		
 		lightbox.on('click',function() {
 			$(this).css('display','none');
-			lightboxContainerAndBg.css('display','none');
+			lightboxContainer.css('display','none');
 		});
 	});
 });
