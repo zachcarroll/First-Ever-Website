8be49cdffa249df2e415c54b3a003761aff76a89
zachcarroll <carroll.zach@gmail.com>
2013-07-11 11:11:46 -0400

    updated

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ $(function() {
 			event.preventDefault();
       	});
 });
+
+$(document).ready(function(){
+	$('.switch').on('click', function(){
+		var text = $("<h2>You better believe it!</h2>")
+		$(this).after(text);	
+		$(this).remove();
+	});
+});
+
+
+  $(document).ready(function() {
+	var headerTop = parseInt($('header').css('height'));
+
+	$(window).scroll(function(){
+		var scrolled = $(window).scrollTop();
+		
+		$('nav').css('background-position','0 '+(-(scrolled))+'px');
+		
+		if (scrolled > headerTop) {
+			$('nav').addClass('fixed');
+		}
+		
+		else {
+			$('nav').removeClass('fixed');
+		}
+	});
+  });
